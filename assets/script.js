0ee3c5cea89aa599635bd9e79312f8fbf0b0602c
Brennan Doehring <brennandoehringd@outlook.com>
2020-09-25 23:03:42 -0400

    Created click events for portfolio button links

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Event handlers for portfolio project button links
+$("#covid19Repo").click(function() {
+    window.open("https://github.com/troylochner/covid19-info");
+});
+
+$("#weatherRepo").click(function() {
+    window.open("https://github.com/brennandoehring/weather-dashboard-bdd");
+})
+
+$("#workdayRepo").click(function() {
+    window.open("https://github.com/brennandoehring/workday-scheduler-bdd");
+})
